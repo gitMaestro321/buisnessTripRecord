b5d205d2eda5afa881ec00b661bfb0b1911c7746
Balsa <balsa.bjelogrlic@gmail.com>
2019-04-16 12:03:16 +0200

    switch to service only

--- src/main/java/com/lab/software/engineering/businesstriporder/service/VehicleService.java
+++ src/main/java/com/lab/software/engineering/businesstriporder/service/VehicleService.java
@@ -2,12 +2,25 @@ package com.lab.software.engineering.businesstriporder.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lab.software.engineering.businesstriporder.dao.VehicleDAO;
 import com.lab.software.engineering.businesstriporder.entity.Vehicle;
 
-public interface VehicleService {
+@Service
+public class VehicleService {
 
-	public List<Vehicle> findAll();
-	public Vehicle addVehicle(Vehicle vehicle);
-	
-	
+	@Autowired
+	private VehicleDAO vehicleDao;
+	 
+	public List<Vehicle> findAll() {
+		return vehicleDao.findAll();
+	} 
+ 
+	public Vehicle addVehicle(Vehicle vehicle) {
+		vehicle.setIsActive(true);
+		vehicle.setIsAvailable(true);
+		return this.vehicleDao.save(vehicle);		
+	}
 }
